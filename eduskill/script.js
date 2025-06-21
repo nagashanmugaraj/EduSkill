21dd8493a0f1a8416ca4a7ea12da71deb3aa343d
Naga Shanmuga Raj <nagashanmugaraj27@gmail.com>
2025-06-21 14:00:02 +0530

    script.js

--- eduskill/script.js
+++ eduskill/script.js
@@ -2,3 +2,12 @@ function sendMessage(event) {
   event.preventDefault();
   alert("Message sent! We’ll get back to you soon.");
 }
+
+function search() {
+  const query = document.getElementById("searchBox").value;
+  alert("You searched for: " + query);
+}
+
+function clearSearch() {
+  document.getElementById("searchBox").value = "";
+}
